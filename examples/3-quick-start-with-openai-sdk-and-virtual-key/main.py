bb7c00a42ef8e12db1d72881f0f500c9ba73eaf8
james <james@obiguard.com>
2025-08-07 12:07:26 +0800

    Insert example with openai SDK.

--- examples/3-quick-start-with-openai-sdk-and-virtual-key/main.py
+++ examples/3-quick-start-with-openai-sdk-and-virtual-key/main.py
@@ -3,19 +3,21 @@ import os
 from openai import OpenAI
 from obiguard import OBIGUARD_GATEWAY_URL, Obiguard
 
+# This example showcases the use of OpenAI SDK with AI providers other than OpenAI.
+
 obiguard_client = Obiguard(
-    obiguard_api_key='sk-obg***',  # Your Obiguard policy group API key
+    obiguard_api_key=os.environ.get("OBIGUARD_API_KEY"),  # Your Obiguard virtual key
     provider='openai',
 )
 
 openai_client = OpenAI(
-    api_key=os.environ.get("OPENAI_API_KEY"),
+    api_key="N/A",  # Not needed when using Obiguard virtual key
     base_url=OBIGUARD_GATEWAY_URL,
     default_headers=obiguard_client.copy_headers()
 )
 
 completion = openai_client.chat.completions.create(
-    model="gpt-4o",
+    model='Qwen/Qwen2.5-32B-Instruct',
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "Hello!"}
