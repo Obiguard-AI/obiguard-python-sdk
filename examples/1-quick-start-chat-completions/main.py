a2eb53b391718e113b492447efa9e0f394d963c2
james <james@obiguard.com>
2025-08-07 11:52:37 +0800

    Cleanups for first example.

--- examples/1-quick-start-chat-completions/main.py
+++ examples/1-quick-start-chat-completions/main.py
@@ -1,14 +1,13 @@
-from obiguard import Obiguard, OBIGUARD_GATEWAY_URL
+from obiguard import Obiguard
 
 client = Obiguard(
-    obiguard_api_key="vk-ob***",  # Your Obiguard API key
+    obiguard_api_key="vk-obg***",  # Your Obiguard virtual key
     provider='openai',
-    base_url=OBIGUARD_GATEWAY_URL,
     strict_open_ai_compliance=False
 )
 
 response = client.chat.completions.create(
-    model = 'Qwen/Qwen2.5-32B-Instruct',
+    model='Qwen/Qwen2.5-32B-Instruct',
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "Hello!"}
