9f5b406e85349ebd36392b3b9ef26193fa02add0
james <james@obiguard.com>
2025-08-07 14:09:49 +0800

    Insert openai async example.

---
+++ examples/7-openai-async-with-obiguard/main.py
@@ -0,0 +1,25 @@
+import asyncio
+import os
+from openai import AsyncOpenAI
+from obiguard import OBIGUARD_GATEWAY_URL, Obiguard
+
+obiguard_client = Obiguard(
+    obiguard_api_key='sk-obg***',  # Your Obiguard policy group API key
+    provider='openai',
+)
+
+openai_client = AsyncOpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+    base_url=OBIGUARD_GATEWAY_URL,
+    default_headers=obiguard_client.copy_headers()
+)
+
+
+async def main() -> None:
+    response = await openai_client.responses.create(
+        model="gpt-4o", input="Explain disestablishmentarianism to a smart five year old."
+    )
+    print(response.output_text)
+
+
+asyncio.run(main())
